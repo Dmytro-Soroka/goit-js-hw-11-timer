d5d06fa863f2500763dd163a625fca9dc0413a71
Dmytro-Soroka <dimasoroka2808@gmail.com>
2020-12-21 15:41:04 +0200

    version 1

---
+++ js/index.js
@@ -0,0 +1,44 @@
+class CountdownTimer {
+  constructor({selector, targetDate}) {
+    this.targetDate = targetDate;
+    this.timer = document.querySelector(selector);
+    this.days = this.timer.querySelector('[data-value="days"]');
+    this.hours = this.timer.querySelector('[data-value="hours"]');
+    this.mins = this.timer.querySelector('[data-value="mins"]');
+    this.secs = this.timer.querySelector('[data-value="secs"]');
+  }
+
+  count() {
+    setInterval(() => {
+      let time = this.targetDate - Date.now();
+      const times = this.getTimeComponents(time);
+      this.updateClock(times);
+    }, 1000)
+  };
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  };
+  
+  pad(value) {
+    return String(value).padStart(2, '0');
+  };
+  
+  updateClock({ days, hours, mins, secs }) {
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+}
+
+const counterUntilNewYear = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jan 01, 2021'),
+});
+
+counterUntilNewYear.count();
